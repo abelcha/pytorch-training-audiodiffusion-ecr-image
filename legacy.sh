899870713cfe209695f2e6bf81a569f6118ed6bd
Abel Chalier <abel.chalier@gmail.com>
2023-01-18 15:19:53 +0100

    first commit

---
+++ legacy.sh
@@ -0,0 +1,36 @@
+# The name of our algorithm
+image=huggingface-training-audiodiffusion
+
+cd container
+
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+# region=$(aws configure get region)
+# region=${region:-us-west-2}
+region="us-west-2"
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
+
+# If the repository doesn't exist in ECR, create it.
+
+aws ecr describe-repositories --repository-names "${image}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${image}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+$(aws ecr get-login --region ${region} --no-include-email)
+
+# Get the login command from ECR in order to pull down the SageMaker PyTorch image
+$(aws ecr get-login --registry-ids 520713654638 --region ${region} --no-include-email)
+
+# Build the docker image locally with the image name and then push it to ECR
+# with the full name.
+
+docker build  -t ${image} . --build-arg REGION=${region}
+docker tag ${image} ${fullname}
+
+docker push ${fullname}
